0275c5f60e1a2829f1834fc9ff7b5f6ee0bb132b
Adam MacDonald <am1macdonald@gmail.com>
2024-02-23 07:55:25 -0500

    Add init and connect methods

--- controllers/DatabaseController.py
+++ controllers/DatabaseController.py
@@ -1,8 +1,20 @@
-import psycopg2
 import os
+
+import psycopg2
+
 
 class DatabaseController:
     def __init__(self):
-        self.connection_string = os.environ["TEST"]
-        print(self.connection_string)
+        print("new DB Controller")
+        self.connection = None
+        self.cursor = None
 
+    def connect(self):
+        self.connection = psycopg2.connect(dbname=os.environ["PGDATABASE"],
+                                user=os.environ["PGUSER"],
+                                password=os.environ["PG_PASSWORD"],
+                                host=os.environ["PGHOST"],
+                                port=os.environ["PGPORT"])
+
+        # Open a cursor to perform database operations
+        self.cursor = self.connection.cursor()
