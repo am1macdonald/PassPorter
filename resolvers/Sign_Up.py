228df8eb91a919df8c1db7366c0da92fc3deaac1
Adam MacDonald <am1macdonald@gmail.com>
2024-02-23 07:37:45 -0500

    Minor updates

--- resolvers/Sign_Up.py
+++ resolvers/Sign_Up.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, Request
+from pydantic import SecretStr, EmailStr
+
+from controllers.DatabaseController import DatabaseController
+from models.User import User
 
 
 class SignupResolver:
-    def __init__(self, request: Request):
-        self.request = request
-        print(request)
+    def __init__(self, email: EmailStr, password: SecretStr, confirm: SecretStr):
+        print(email)
+        print(password.get_secret_value())
+        DatabaseController()
+        User('bob', 'bobspass')
